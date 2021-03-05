d4a47aef3a2722922a0aa4b407dab926b6309ec6
linsongwang <linsongwang@chuanglintech.com>
2021-03-05 09:49:04 +0800

    补充部分mediatype

---
+++ nineya-core/src/main/java/com/nineya/tool/http/MediaType.java
@@ -0,0 +1,25 @@
+package com.nineya.tool.http;
+
+/**
+ * 文件类型
+ * @author 殇雪话诀别
+ */
+public class MediaType {
+    public static final String ALL = "*/*";
+
+    public static final String APPLICATION_RSS_XML = "application/rss+xml";
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    public static final String APPLICATION_XML = "application/xml";
+
+    public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    public static final String IMAGE_JPEG = "image/jpeg";
+
+    public static final String IMAGE_PNG = "image/png";
+
+    public static final String IMAGE_GIF = "image/gif";
+
+    public static final String TEXT_HTML = "text/html";
+}
