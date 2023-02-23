84311587296ab7864b7bf7c7f05f6a34f38f6112
nineya <lsw@nineya.com>
2023-02-23 22:46:44 +0800

    feat(jwtToken): 添加构造方法

--- authentication-spring-boot-starter/src/main/java/com/nineya/authentication/isp/AuthLoginType.java
+++ authentication-spring-boot-starter/src/main/java/com/nineya/authentication/isp/AuthLoginType.java
@@ -1,6 +1,8 @@
 package com.nineya.authentication.isp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 默认的领域登录类型
@@ -9,6 +11,8 @@ import lombok.Data;
  * @date 2022/12/17 1:31
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthLoginType implements LoginType {
     private String realmName;
     private String headerName;
